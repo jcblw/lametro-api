5a19f38e610cf42d779f260cf1c8e7230585f885
jcblw <jacoblowe2.0@gmail.com>
2017-07-05 19:36:26 -0700

    add in rail endpoint :tada:

--- index.js
+++ index.js
@@ -11,19 +11,14 @@ const ENDPOINTS = [
   'agencies/:agency/routes/:id/vehicles/'
 ]
 const agencies = {
-  bus: 'lametro'
-  /*
-    currently the rail apis do not work
-    seems to just return bus info
-    rail: 'lametro-rail'
-  */
+  bus: 'lametro',
+  rail: 'lametro-rail'
 }
 const getEndpointMethodName = endpoint => {
-  return endpoint.split(/\//)
-    .reduce((accum, segment) => {
-      if (segment.match(/^:/) || segment === 'agencies') return accum
-      return accum ? `${accum}${toTitleCase(segment)}` : segment
-    }, '')
+  return endpoint.split(/\//).reduce((accum, segment) => {
+    if (segment.match(/^:/) || segment === 'agencies') return accum
+    return accum ? `${accum}${toTitleCase(segment)}` : segment
+  }, '')
 }
 const toTitleCase = str => {
   return str.substr(0, 1).toUpperCase() + str.substr(1)
@@ -38,24 +33,28 @@ const request = (endpoint, agency) => (options = {}, callback) => {
     .replace(':agency', agency)
     .replace('//', '/')
   const url = `${BASE_URL}${_endpoint}`
-  const req = got.get(url, {json: true})
+  const req = got.get(url, { json: true })
   if (typeof callback !== 'function') return req
-  req
-    .then(resp => callback(null, resp.body))
-    .catch(callback)
+  req.then(resp => callback(null, resp.body)).catch(callback)
 }
-const metro = ENDPOINTS.reduce((accum, endpoint) => {
-  Object.keys(agencies).forEach(agency => {
-    if (typeof accum[agency] === 'undefined') {
-      accum[agency] = {}
-    }
-    accum[agency][getEndpointMethodName(endpoint)] = request(endpoint, agencies[agency])
-  })
-  return accum
-}, {
-  request,
-  toTitleCase,
-  getEndpointMethodName
-})
+const metro = ENDPOINTS.reduce(
+  (accum, endpoint) => {
+    Object.keys(agencies).forEach(agency => {
+      if (typeof accum[agency] === 'undefined') {
+        accum[agency] = {}
+      }
+      accum[agency][getEndpointMethodName(endpoint)] = request(
+        endpoint,
+        agencies[agency]
+      )
+    })
+    return accum
+  },
+  {
+    request,
+    toTitleCase,
+    getEndpointMethodName
+  }
+)
 
 module.exports = metro
